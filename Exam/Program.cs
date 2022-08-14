a065e1ecaffc0256a7adf29e8816c38fd404c94f
igor <igor.borodin.888@gmail.com>
2022-08-14 16:53:25 +0300

    Описание задачи

---
+++ Exam/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+string[] myArray = new string[3] { "Hi", "world", ":)" };
+Console.Write("[" + string.Join(",", myArray) + "]");
+Console.Write(" -> ");
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i].Length <= 3)
+    {    
+         Console.Write($"[{myArray[i]}] ");
+    }
+
+}
+
+
+
+
+
+
+
